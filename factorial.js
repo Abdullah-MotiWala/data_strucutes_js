64ff19a89f7b5b12cf33320dc7c9b622e680e3c8
Abdullah MotiWala <abdullah.motiwala@takafulbazaar.pk>
2024-02-09 14:42:16 +0500

    algo: factorial

---
+++ factorial.js
@@ -0,0 +1,43 @@
+const { TimeComplexities } = require("./index");
+
+// Method 1
+function findFactorial(n) {
+  const isNegativeNumber = n < 0;
+  if (isNegativeNumber)
+    throw new Error("Unable to find factorial of negative number");
+
+  let factorial = 1;
+  const isZeroNumber = n === 0;
+  if (!isZeroNumber) {
+    for (let i = 2; i <= n; i++) {
+      factorial *= i;
+    }
+  }
+
+  return factorial;
+}
+
+const result = findFactorial(4);
+const complexity = { time: TimeComplexities.LINEAR_LOOP };
+console.log("method 1:", { result, complexity });
+
+function findFactorial2(n) {
+  const isNegativeNumber = n < 0;
+  if (isNegativeNumber)
+    throw new Error("Unable to find factorial of negative number");
+
+  let factorial = 1;
+  const isZeroNumber = n === 0;
+  if (!isZeroNumber) {
+    for (let i = n; i > 1; i--) {
+      factorial *= i;
+    }
+  }
+
+  return factorial;
+}
+
+const result2 = findFactorial2(4);
+const complexity2 = { time: TimeComplexities.LINEAR_LOOP };
+
+console.log("method 2:", { result2, complexity2 });
