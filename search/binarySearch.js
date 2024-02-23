95ddb4c119a3c06007c6b9328115f3e5845898ca
Abdullah MotiWala <abdullah.motiwala@takafulbazaar.pk>
2024-02-23 15:07:32 +0500

    binary search recursive

--- search/binarySearch.js
+++ search/binarySearch.js
@@ -23,7 +23,34 @@ function findElementIndex(ele, arr) {
   return elementIndex;
 }
 
-const result = findElementIndex(61, [1, 2, 3, 4, 5, 6, 58, 60, 61]);
+const result = findElementIndex(6, [1, 2, 3, 4, 5, 6, 58, 60, 61]);
 const complexity = { time: TimeComplexities.REDUCED_BY_HALF };
 
 console.log({ result, complexity });
+
+function findElementIndex2(num, arr) {
+  let elementIndex = -1;
+  function findElementIndexRecursive(firstInd = 0, lastInd = arr.length - 1) {
+    const midIndex = Math.floor((firstInd + lastInd) / 2);
+
+    if (arr[midIndex] === num) {
+      elementIndex = midIndex;
+      return;
+    }
+
+    if (num > midIndex) {
+      findElementIndexRecursive(midIndex + 1, lastInd);
+    } else {
+      findElementIndexRecursive(firstInd, midIndex - 1);
+    }
+  }
+
+  findElementIndexRecursive();
+
+  return elementIndex;
+}
+
+const result2 = findElementIndex2(6, [1, 2, 3, 4, 5, 6, 58, 60, 61]);
+const complexity2 = { time: TimeComplexities.REDUCED_BY_HALF };
+
+console.log({ result2, complexity2 });
